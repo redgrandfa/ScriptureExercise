e8e53c2239846b2357b1df877da2313a186ed3d8
redgrandfa <83260277+redgrandfa@users.noreply.github.com>
2023-02-14 00:14:04 +0800

    feat 收藏清單若為空的時，顯示提示文字

--- ScriptureExercise/wwwroot/js/Exercise/collectList.js
+++ ScriptureExercise/wwwroot/js/Exercise/collectList.js
@@ -9,29 +9,56 @@ let vue_collectList = new Vue({
             resp.json().then((respBody) => {
                 let collectList = respBody.payload;
 
+                //照收藏順序的寫法
+                collectList.forEach( subjectCode => {
+                    let scriptureCode = subjectCode[0]
+                    let subjectId = parseInt(subjectCode[1])
+                    // let subjectId = subjectCode[1]
+
+                    let scripture = scriptures_inDB
+                        .find(scripture => scripture.code == scriptureCode)
+
+                    let subject = scripture.subjects
+                        .find(subject => subject.id == subjectId)
+
+                    let subjectToAdd = {
+                        scripture: scripture.code,
+                        scriptureTitle: scripture.title,
+                        coverImgFile:scripture.coverImgFile,
+                        author: scripture.author,
+                        belongTo: scripture.belongTo,
+    
+                        id: subject.id,
+                        title: getSubjectTitle(scripture.code, subject.id),
+                        isCollected: true,
+                    };
+    
+                    this.subjects.push(subjectToAdd);
+                })
+
                 //載入 所有經典資料  攤平成科目
-                scriptures_inDB.forEach((scripture) => {
-                    scripture.subjects.forEach((subject) => {
-                        let subjectCode = `${scripture.code}${subject.id}`
+                // scriptures_inDB.forEach((scripture) => {
+                //     scripture.subjects.forEach((subject) => {
+                //         let subjectCode = `${scripture.code}${subject.id}`
         
-                        //只需要有收藏的
-                        if(collectList.includes(subjectCode)){
-                            let subjectToAdd = {
-                                scripture: scripture.code,
-                                scriptureTitle: scripture.title,
-                                coverImgFile:scripture.coverImgFile,
-                                author: scripture.author,
-                                belongTo: scripture.belongTo,
+                //         //只需要有收藏的
+                //         if(collectList.includes(subjectCode)){
+                //             let subjectToAdd = {
+                //                 scripture: scripture.code,
+                //                 scriptureTitle: scripture.title,
+                //                 coverImgFile:scripture.coverImgFile,
+                //                 author: scripture.author,
+                //                 belongTo: scripture.belongTo,
             
-                                id: subject.id,
-                                title: getSubjectTitle(scripture.code, subject.id),
-                                isCollected: true,
-                            };
+                //                 id: subject.id,
+                //                 title: getSubjectTitle(scripture.code, subject.id),
+                //                 isCollected: true,
+                //             };
             
-                            this.subjects.push(subjectToAdd);
-                        }
-                    })
-                });
+                //             this.subjects.push(subjectToAdd);
+                //         }
+                //     })
+                // });
             });
         });
     },
